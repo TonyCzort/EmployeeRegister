0a411db74ab3ee483b46ca9958f8823f343cbc15
Marcin Choszcz <marcinchoszcz@gmail.com>
2019-11-07 18:16:37 +0100

    Template

---
+++ EmployeeRegister_CRUD_MVC_EFCore/Models/EmployeeContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeRegister_CRUD_MVC_EFCore.Models
+{
+    public class EmployeeContext:DbContext
+    {
+        public EmployeeContext()
+        {
+
+        }
+
+    }
+}
