afeff57ab122dc84780c523b744dfddc7db1e5c2
Marcin Choszcz <marcinchoszcz@gmail.com>
2019-11-07 16:15:15 +0100

    Add project files.

---
+++ EmployeeRegister_CRUD_MVC_EFCore/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace EmployeeRegister_CRUD_MVC_EFCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
